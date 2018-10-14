0a15bb4fecbaf306c09d472a1c99e4ce5d1417a9
Vishal Amrutiya <vishalvamrutiya@gmail.com>
2018-10-15 01:39:18 +0530

    Initial commit

---
+++ MicroService/Image/src/main/java/com/eureka/image/ImageApplication.java
@@ -0,0 +1,14 @@
+package com.eureka.image;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient // Enable eureka client. It inherits from @EnableDiscoveryClient.
+public class ImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageApplication.class, args);
+	}
+}
